7343d93b8128e395b4c95c967f4a77e627daaf8a
Mehtaab Aalam <124188175+Mehtaab11@users.noreply.github.com>
2026-06-13 22:42:22 +0530

    Add: word-ladder - submission-8

---
+++ Data Structures & Algorithms/word-ladder/submission-8.py
@@ -0,0 +1,35 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+
+        wordList.append(beginWord)
+        nei = collections.defaultdict(list)
+
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j + 1 :]
+                nei[pattern].append(word)
+
+        q = deque([beginWord])
+        visited = set([beginWord])
+
+        ans = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+
+                if word == endWord:
+                    return ans
+
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j + 1 :]
+
+                    for curr in nei[pattern]:
+                        if curr not in visited:
+                            visited.add(curr)
+                            q.append(curr)
+            ans += 1
+
+        # the code will reach here only if the word can't be connected using the wordList
+        return 0
