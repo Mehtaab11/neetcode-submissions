36abd7e6f20555439ae5d12cc1a2d9be8fcd0bac
Mehtaab Aalam <mehtaabaalam117@gmail.com>
2026-07-19 19:13:35 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        target = Counter(t)
+
+        mp = defaultdict(int)
+
+        required = len(target)
+        
+        formed = 0
+
+        best = float("inf")
+
+        left_best = 0
+        right_best = 0
+
+        left = 0
+
+        for right in range(len(s)):
+            char = s[right]
+            mp[char] += 1
+
+            if char in target and mp[char] == target[char]:
+                formed += 1
+
+            while formed == required:
+                if right - left + 1 < best:
+                    best = right - left + 1
+                    left_best = left
+                    right_best = right
+
+                left_char = s[left]
+                mp[left_char] -= 1
+
+                if left_char in target and mp[left_char] < target[left_char]:
+                    formed -= 1
+
+                left += 1
+
+        if best == float("inf"):
+            return ""
+
+      
+        return s[left_best : right_best + 1]
