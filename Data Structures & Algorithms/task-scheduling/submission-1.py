cd902885c016eaff23ddc815804cd508bea49cb4
Mehtaab Aalam <mehtaabaalam117@gmail.com>
2026-07-27 22:41:01 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+
+        count = Counter(tasks)
+        max_heap = [-cnt for cnt in count.values()]
+        heapq.heapify(max_heap)
+        # stores the freqency of Letters
+        time = 0
+        q = deque() # [-cnt , idletime]
+
+        while max_heap or q:
+            time += 1
+            if max_heap:
+                # cnt = 1 - -heapq.heappop(max_heap)
+                cnt = 1 + heapq.heappop(max_heap)
+                if cnt:
+                    q.append([cnt , time + n ])
+            # This is the neat and important part
+            if q and q[0][1] == time:
+                heapq.heappush(max_heap, q.popleft()[0])
+
+        return time
+
