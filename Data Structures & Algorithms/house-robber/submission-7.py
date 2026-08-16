fee295a682fd9e52726e6665c33cf0be884543b7
Mehtaab Aalam <mehtaabaalam117@gmail.com>
2026-08-16 22:24:03 +0530

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        maxi = float("-inf")
+        memo  = {}
+        def dfs(idx):
+            nonlocal maxi
+            if idx >= len(nums):
+                return 0
+            if idx in memo :
+                return memo[idx]
+
+            skip = dfs(idx + 1)
+            steal = dfs(idx + 2)
+
+            maxi = max(steal + nums[idx], skip)
+            memo[idx] = maxi
+            return memo[idx]
+
+        dfs(0)
+
+        return maxi
